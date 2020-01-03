97377f8caad50e581b4d3a0d70c7a629d256dca4
Ma Chun <chun.ma@qq.com>
2020-01-03 14:23:01 +0800

    add Demo, the integration activemq with spring.

---
+++ java/net/atos/sample/activemq/StartApplication.java
@@ -0,0 +1,17 @@
+﻿package net.atos.sample.activemq;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StartApplication {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-context-activemq.xml");
+		MessageService messageService = (MessageService) ctx.getBean("messageService");
+		messageService.sendQueueMessage("我的测试消息1");
+		messageService.sendQueueMessage("我的测试消息2");
+		messageService.sendQueueMessage("我的测试消息3");
+	}
+
+}
